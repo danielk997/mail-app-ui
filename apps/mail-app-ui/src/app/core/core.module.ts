8dc5f249ff44fa4a1d4ce8e5dab3dc7be507a6b3
Daniel Kociolek <daniel.kociolek@hawatel.com>
2022-11-13 12:54:52 +0100

    Add header component

--- apps/mail-app-ui/src/app/core/core.module.ts
+++ apps/mail-app-ui/src/app/core/core.module.ts
@@ -1,12 +1,12 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {LoginComponent} from "./components/login/login.component";
-
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
-  declarations: [LoginComponent],
-  imports: [BrowserModule],
-  exports: [LoginComponent]
+  declarations: [LoginComponent, HeaderComponent],
+  imports: [BrowserModule, SharedModule],
+  exports: [LoginComponent, HeaderComponent],
 })
-export class CoreModule {
-}
+export class CoreModule {}
--- apps/mail-app-ui/src/app/shared/components/materials.ts
+++ apps/mail-app-ui/src/app/shared/components/materials.ts
@@ -1,5 +1,7 @@
 import {MatInputModule} from "@angular/material/input";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 export const MATERIALS = [
   MatInputModule,
+  MatToolbarModule,
 ]
